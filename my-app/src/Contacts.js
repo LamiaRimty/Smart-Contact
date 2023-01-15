07bab98cd7056ca8d6a62a4aeadc7bd870a7096d
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-15 21:51:44 +0100

    Create Detail.jsx comp to reuse informations of contacts

--- my-app/src/Contacts.js
+++ my-app/src/Contacts.js
@@ -1,17 +1,19 @@
 const contacts = [
-    {
+    { id:1,
       name: "Jaya Ahsan",
       img: "https://i.pinimg.com/564x/73/16/aa/7316aa46e85f0d0c06768a76f7330eac.jpg",
       tel: "+1788787",
       email: "jaya@gmail.com"
     },
     {
+      id:2,
       name: "Chanchal Chowdhury",
       img: "https://www.themoviedb.org/t/p/w235_and_h235_face/kdekgPF8Z3thvmK8QKvBhEhcuYN.jpg",
       tel: "+0172376778",
       email: "chan@gmail.com"
     },
     {
+      id:3,
       name: "Siam Ahmed",
       img:"https://www.themoviedb.org/t/p/w235_and_h235_face/4gSLuSAkUMnjv0nRRgUjOGSo2YU.jpg",
       tel: "+017 567 778",
